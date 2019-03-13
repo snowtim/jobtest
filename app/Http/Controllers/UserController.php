5c482fb20dca0c20b8bb79161692a333df48c3f4
Your Name <you@example.com>
2019-03-13 21:33:29 +0800

    Job test
    job test

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\User;
+
+class UserController extends Controller
+{
+
+    protected function create(Request $request) {
+
+    	$validator = Validator::make($request->all(), [
+    		'Account' => ['required', 'string', 'max:50'],
+    		'Password' => ['required', 'string', 'max:50']
+    	]);
+
+    	if($validator->fails()) {
+    		return response()->json([
+    			'result' => 'false',
+    			'response' => $validator->errors()->first()
+    		]);	
+    	}
+
+    	User::create([
+    		'account' => $request['Account'],
+    		'password' => $request['Password']
+    	]);
+
+    	return response()->json([
+    		'Code' => 0,
+    		'Message' => "",
+    		'Result' => [
+    			'IsOK' => true
+    		]
+    	]);
+
+    }
+
+    protected function delete(Request $request) {
+
+  	  	$validator = Validator::make($request->all(), [
+    		'Account' => ['required', 'string', 'max:50'],
+    	]);
+
+    	if($validator->fails()) {
+    		return response()->json([
+    			'result' => 'false',
+    			'response' => $validator->errors()->first()
+    		]);	
+    	}
+
+    	User::where('account', $request['Account'])->delete();
+
+    	return response()->json([
+    		'Code' => 0,
+    		'Message' => "",
+    		'Result' => [
+    			'IsOK' => true
+    		]
+    	]);
+    	
+    }
+
+    protected function update(Request $request) {
+    	
+    	$validator = Validator::make($request->all(), [
+    		'Account' => ['required', 'string', 'max:50'],
+    		'Password' => ['required', 'string', 'max:50']
+    	]);
+
+    	if($validator->fails()) {
+    		return response()->json([
+    			'result' => 'false',
+    			'response' => $validator->errors()->first()
+    		]);	
+    	}
+
+    	User::where('account', $request['Account'])->update([
+    		'password' => $request['Password']
+    	]);
+
+    	return response()->json([
+    		'Code' => 0,
+    		'Message' => "",
+    		'Result' => [
+    			'IsOK' => true
+    		]
+    	]);
+
+    }
+
+    protected function login(Request $request) {
+
+    	$validator = Validator::make($request->all(), [
+    		'Account' => ['required', 'string', 'max:50'],
+    		'Password' => ['required', 'string', 'max:50']
+    	]);
+
+    	if($validator->fails()) {
+    		return response()->json([
+    			'result' => 'false',
+    			'response' => $validator->errors()->first()
+    		]);	
+    	}
+
+    	$userinfo = User::where('account', $request['Account'])->get()->first();
+
+    	if($userinfo != null) {
+    		if($userinfo['account'] == $request['Account'] && $userinfo['password'] == $request['Password']) {
+    			return response()->json([
+    				'Code' => 0,
+    				'Message' => "",
+    				'Result' => 200
+    			]);
+    		} 
+    			return response()->json([
+    				'Code' => 2,
+    				'Message' => "Login Failed",
+    				'Result' => 400
+    			]);
+    		
+    	}
+    	return response()->json([
+    		'Code' => 2,
+    		'Message' => "Login Failed",
+    		'Result' => 400
+    		]);
+
+    }
+
+}
